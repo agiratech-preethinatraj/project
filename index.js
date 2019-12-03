56003b383a36761ffe51ce3aa2654b9d5d2ede3a
preethi-ntrajan <preethi.natarajan@gmail.com>
2019-12-03 10:36:21 +0530

    week added

--- index.js
+++ index.js
@@ -2,17 +2,14 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "preethi@123",
-  database: "db"});
+  password: "",
+  database: "mydb"});
 con.connect();
-
-//function for put method
-function put (req, callback,mes){
-  var sql = "UPDATE week SET agenda =('"+req.body.agenda+ "') WHERE user_id=('"+req.body.user_id+"') ";
+function put (req, callback){
+  var sql = "UPDATE events SET agenda =('"+req.body.agenda+ "') WHERE user_id=('"+req.body.user_id+"') ";
    con.query(sql, function (err, result) {
      var response = {}
      if(err){
-       console.log(err)
       response.msg = "error"
       callback(400, response)
       }else{
@@ -23,24 +20,21 @@ function put (req, callback,mes){
   });
   }
 
-//function for post method
-  function post  (req, callback,mes) {
-   con.query('INSERT INTO week (user_id, date,agenda,start_time,end_time,mode,status)   values (("'+req.body.user_id+'"),("'+req.body.date+'"),("'+req.body.agenda+'"),("'+req.body.start_time+'"),("'+req.body.end_time+'"),("'+req.body.mode+'"),("'+req.body.status+'"))', function (err, result) {
-     var res={}
-     if(err){
+  function post  (req, callback) {
+    con.query('INSERT INTO events (user_id, date,agenda,start_time,end_time,mode,status)   values (("'+req.body.user_id+'"),("'+req.body.date+'"),("'+req.body.agenda+'"),("'+req.body.start_time+'"),("'+req.body.end_time+'"),("'+req.body.mode+'"),("'+req.body.status+'"))', function (err, result) {
+  var res={}
+      if(err){
         res.msg = "error"
           callback(400,res)
           }else{
           console.log("one item added");
-          res.msg = ("no of item added:"+result.affectedRows)
+          res.msg=("item added")
           callback(200,res)
         }
       });
   };
-
-//function for delete method
-function del(req,callback,mes){
-  var sql =  "DELETE from week WHERE agenda = ('"+req.body.agenda+"')";
+function del(req,callback){
+  var sql =  "DELETE from events WHERE agenda = ('"+req.body.agenda+"')";
   con.query(sql, function (err, res) {
     var res={}
     if(err){
@@ -48,26 +42,29 @@ function del(req,callback,mes){
         callback(400,res)
         }else{
         console.log("one item deleted");
-        res.msg = ("no of item deleted:"+result.affectedRows)
+        res.msg=("item deleted")
         callback(200,res)
       }
   });
 };
 
-//function for get method
-function get(req,callback,mes){
-    con.query("SELECT * from week", function(err,result){
+function get(req,callback){
+    con.query("SELECT * from events", function(err,result){
   var response={}
   if(err){
     response.msg = "error"
     callback(400,response)
   }else{
+
     var json ={}
     var status = {}
+
     past = []
     present = []
     future = []
-  for (r in result){
+
+
+    for (r in result){
       if(result[r].status == 'true'){
         ch = {}
         ch.user_id  = result[r].user_id;
@@ -78,6 +75,7 @@ function get(req,callback,mes){
         ch.mode     = result[r].mode;
         ch.status   = result[r].status;
         past.push(ch);
+        //json.past[r] = ch
         console.log(ch)
       }
 
@@ -91,6 +89,7 @@ function get(req,callback,mes){
         ch.mode     = result[r].mode;
         ch.status   = result[r].status;
         present.push(ch)
+        //json.future[r] = ch
         console.log(ch)
       }
 
@@ -104,21 +103,22 @@ function get(req,callback,mes){
         ch.mode     = result[r].mode;
         ch.status   = result[r].status;
         future.push(ch)
+        //json.present[r] = ch;
         console.log(ch)
       }
     }
-    json.week ={}
-    json.week.past = past;
-    json.week.present = present;
-    json.week.future = future;
+    json.month ={}
+
+    json.month.past = past;
+    json.month.present = present;
+    json.month.future = future;
     console.log(json);
-    response.msg=(json)
-    callback(200,response)
+      //response.send(json);
+      response.msg=(json)
+      callback(200,response)
 };
 });
 }
-
-//exporting the function
 module.exports = {
   put:put,
   post:post,
